47b3fc36e5a7441019cb78d1083607a40258b9df
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2023-04-06 14:35:31 +0700

    Add files via upload

---
+++ PlaneTicket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    public class PlaneTicket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin\t\t\t: {0}", Origin);
+            Console.WriteLine("Destination\t\t: {0}", Destination);
+            Console.WriteLine("Cost\t\t\t: {0} {1}", Cost, Currency);
+
+            if (Currency == "(USD)")
+            {
+                Console.WriteLine("Convert to Rupiah\t: {0} ", (Cost * 15000));
+            }
+            else if (Currency == "(IDR)")
+            {
+                Console.WriteLine("Convert to Dollar\t: {0} ", Cost / 15000);
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket satu = new PlaneTicket();
+            PlaneTicket dua = new PlaneTicket();
+
+            satu.Origin = "JGJ";
+            satu.Destination = "PKU";
+            satu.Cost = 100;
+            satu.Currency = "(USD)";
+            satu.PrintSummary();
+
+            Console.WriteLine();
+
+            dua.Origin = "JKT";
+            dua.Destination = "PKU";
+            dua.Cost = 750000;
+            dua.Currency = "(IDR)";
+
+            dua.PrintSummary();
+            Console.ReadKey();
+        }
+    }
+}
